20cdaea96ccac696b540c8b8af3ea57ac78fa8ff
Kevin Baluha <kevin@thetaalchemy.com>
2017-12-19 04:47:52 -0700

    coverage

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-process.env.COVERAGE = "true";
-
-let im = require('istanbul-middleware');
-
-let isCoverageEnabled = (process.env.COVERAGE == "true"); 
- 
-if (isCoverageEnabled) {
-    console.log('coverage enabled');
-    im.hookLoader(__dirname);
-    }
- 
 let express = require('express');
 let app = express();
 
-if (isCoverageEnabled) {
-    app.use('/coverage', im.createHandler());
-    }
- 
 let port = 8080;
+
 let terms = require('./app/routes/terms');
 
 app.route("/terms/:tid/longest-preview-media-url")
